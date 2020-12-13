c47132f4de9492a7d87bbb294e2c339e93ab4e09
yolosunqi <72311163+yolosunqi@users.noreply.github.com>
2020-12-13 18:19:21 +0800

    第十次和第十一次

---
+++ Ten(1)/Ten/app/src/main/java/com/example/ten/constant/ImageList.java
@@ -0,0 +1,17 @@
+package com.example.ten.constant;
+
+import com.example.ten.R;
+
+import java.util.ArrayList;
+
+public class ImageList {
+    public static ArrayList<Integer> getDefault() {
+        ArrayList<Integer> imageList = new ArrayList<Integer>();
+        imageList.add(R.drawable.a1);
+        imageList.add(R.drawable.a2);
+        imageList.add(R.drawable.a3);
+        imageList.add(R.drawable.a4);
+        imageList.add(R.drawable.a5);
+        return imageList;
+    }
+}
---
+++ homework10/app/src/main/java/com/example/homework10/constant/ImageList.java
@@ -0,0 +1,16 @@
+package com.example.homework10.constant;
+
+import com.example.homework10.R;
+
+import java.util.ArrayList;
+
+public class ImageList {
+    public static ArrayList<Integer> getDefault() {
+        ArrayList<Integer> imageList = new ArrayList<Integer>();
+        imageList.add(R.drawable.banner_1);
+        imageList.add(R.drawable.banner_2);
+        imageList.add(R.drawable.banner_3);
+        imageList.add(R.drawable.banner_4);
+        return imageList;
+    }
+}
