5e0a81035dbb1a1c6279376c2f5208b51182a1f3
yolosunqi <72311163+yolosunqi@users.noreply.github.com>
2020-12-19 12:39:16 +0800

    finalwork

---
+++ homework12 2/app/src/main/java/com/example/homework12/bean/GoodsInfo.java
@@ -0,0 +1,124 @@
+package com.example.homework12.bean;
+
+import com.example.homework12.R;
+
+import java.util.ArrayList;
+
+public class GoodsInfo {
+    public long rowid; // 行号
+    public int sn; // 序号
+    public String name; // 名称
+    public String desc1; // 描述
+    public float price; // 价格
+    public String thumb_path; // 小图的保存路径
+    public String pic_path; // 大图的保存路径
+    public int thumb; // 小图的资源编号
+    public int pic; // 大图的资源编号
+    public int pic_id;
+    public String title;
+    public boolean bPressed;
+    public int id;
+    private static int seq = 0;
+    public String desc;
+    public GoodsInfo(int pic_id, String title, String desc) {
+        rowid = 0L;
+        sn = 0;
+        name = "";
+        desc1 = "";
+        price = 0;
+        thumb_path = "";
+        pic_path = "";
+        thumb = 0;
+        pic = 0;
+        this.pic_id = pic_id;
+        this.title = title;
+        this.desc = desc;
+        this.bPressed = false;
+        this.id = this.seq;
+        this.seq++;
+    }
+
+    // 声明一个手机商品的名称数组
+    private static String[] mNameArray = {
+            "iPhone 12 promax", "华为mate40 pro", "小米10", "oppo reno4 SE", "vivo 黑曜石", "魅族17"
+    };
+    // 声明一个手机商品的描述数组
+    private static String[] mDescArray = new String[]{
+            "值享焕新版 Apple iPhone 12 promax 128G 金色",
+            "华为mate40 pro 5G手机 亮黑色 8+256G 全网通",
+            "现货速发 小米10 5G手机 骁龙865 1亿像素游戏手机新品1",
+            "oppo reno4 SE 5G新品手机 全面屏 拍照 全网通",
+            "vivo 6GB+64GB 黑曜石 5000mAh大电池 骁龙662处理器",
+            "魅族17 8GB+128GB 十七度灰 骁龙865"
+    };
+    // 声明一个手机商品的价格数组
+    private static float[] mPriceArray = {6666, 4356, 3211, 2999, 2455, 2666};
+    // 声明一个手机商品的小图数组
+    private static int[] mThumbArray = {
+            R.drawable.iphone_s, R.drawable.huawei_s, R.drawable.xiaomi_s,
+            R.drawable.oppo_s, R.drawable.vivo_s, R.drawable.meizu_s,
+    };
+    // 声明一个手机商品的大图数组
+    private static int[] mPicArray = {
+            R.drawable.iphone, R.drawable.huawei, R.drawable.xiaomi,
+            R.drawable.oppo, R.drawable.vivo, R.drawable.meizu,
+    };
+
+    public GoodsInfo() {
+        rowid = 0L;
+        sn = 0;
+        name = "";
+        desc1 = "";
+        price = 0;
+        thumb_path = "";
+        pic_path = "";
+        thumb = 0;
+        pic = 0;
+    }
+
+    // 获取默认的手机信息列表
+    public static ArrayList<GoodsInfo> getDefaultList() {
+        ArrayList<GoodsInfo> goodsList = new ArrayList<GoodsInfo>();
+        for (int i = 0; i < mNameArray.length; i++) {
+            GoodsInfo info = new GoodsInfo();
+            info.name = mNameArray[i];
+            info.desc = mDescArray[i];
+            info.price = mPriceArray[i];
+            info.thumb = mThumbArray[i];
+            info.pic = mPicArray[i];
+            goodsList.add(info);
+        }
+        return goodsList;
+    }
+    private static int[] gridImageArray = {R.drawable.pic_01, R.drawable.pic_02, R.drawable.pic_03
+            , R.drawable.pic_04, R.drawable.pic_05, R.drawable.pic_06, R.drawable.pic_07
+            , R.drawable.pic_08, R.drawable.pic_09, R.drawable.pic_10};
+    private static String[] gridTitleArray = {"商场", "超市", "百货", "便利店",
+            "地摊", "食杂店", "饭店", "餐厅", "会所", "菜市场"};
+
+    public static ArrayList<GoodsInfo> getDefaultGrid() {
+        ArrayList<GoodsInfo> gridArray = new ArrayList<GoodsInfo>();
+        for (int i = 0; i < gridImageArray.length; i++) {
+            gridArray.add(new GoodsInfo(gridImageArray[i], gridTitleArray[i], null));
+        }
+        return gridArray;
+    }
+    private static int[] stagImageArray = {R.drawable.skirt01, R.drawable.skirt02, R.drawable.skirt03
+            , R.drawable.skirt04, R.drawable.skirt05, R.drawable.skirt06, R.drawable.skirt07
+            , R.drawable.skirt08, R.drawable.skirt09, R.drawable.skirt10, R.drawable.skirt11
+            , R.drawable.skirt12, R.drawable.skirt13, R.drawable.skirt14, R.drawable.skirt15
+            , R.drawable.skirt16, R.drawable.skirt17, R.drawable.skirt18, R.drawable.skirt19
+            , R.drawable.skirt20, R.drawable.skirt21, R.drawable.skirt22, R.drawable.skirt23};
+    private static String[] stagTitleArray = {"促销价", "惊爆价", "跳楼价", "白菜价", "清仓价", "割肉价",
+            "实惠价", "一口价", "满意价", "打折价", "腰斩价", "无人问津", "算了吧", "大声点",
+            "嘘嘘", "嗯嗯", "呼呼", "呵呵", "哈哈", "嘿嘿", "嘻嘻", "嗷嗷", "喔喔"};
+
+    public static ArrayList<GoodsInfo> getDefaultStag() {
+
+        ArrayList<GoodsInfo> stagArray = new ArrayList<GoodsInfo>();
+        for (int i = 0; i < stagImageArray.length; i++) {
+            stagArray.add(new GoodsInfo(stagImageArray[i], stagTitleArray[i], null));
+        }
+        return stagArray;
+    }
+}
